6290468ddeccb495f88335108a22be4932e2f731
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2019-11-19 06:44:58 +0500

    fr

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,110 @@
+/* route */
+
+'use stict';
+
+function Route(name, htmlName, defaultRoute) {
+    try {
+        if(!name || !htmlName) {
+            throw 'error: name and htmlName params are mandatories';
+        }
+        this.constructor(name, htmlName, defaultRoute);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+Route.prototype = {
+    name: undefined,
+    htmlName: undefined,
+    default: undefined,
+    constructor: function (name, htmlName, defaultRoute) {
+        this.name = name;
+        this.htmlName = htmlName;
+        this.default = defaultRoute;
+    },
+    isActiveRoute: function (hashedPath) {
+        return hashedPath.replace('#', '') === this.name; 
+    }
+};
+
+
+
+/* router */
+'use strict';
+
+function Router(routes) {
+    try {
+        if (!routes) {
+            throw 'error: routes param is mandatory';
+        }
+        this.constructor(routes);
+        this.init();
+    } catch (e) {
+        console.error(e);   
+    }
+}
+
+Router.prototype = {
+    routes: undefined,
+    rootElem: undefined,
+    constructor: function (routes) {
+        this.routes = routes;
+        this.rootElem = document.getElementById('app');
+    },
+    init: function () {
+        var r = this.routes;
+        (function(scope, r) { 
+            window.addEventListener('hashchange', function (e) {
+                scope.hasChanged(scope, r);
+            });
+        })(this, r);
+        this.hasChanged(this, r);
+    },
+    hasChanged: function(scope, r){
+        if (window.location.hash.length > 0) {
+            for (var i = 0, length = r.length; i < length; i++) {
+                var route = r[i];
+                if(route.isActiveRoute(window.location.hash.substr(1))) {
+                    scope.goToRoute(route.htmlName);
+                }
+            }
+        } else {
+            for (var i = 0, length = r.length; i < length; i++) {
+                var route = r[i];
+                if(route.default) {
+                    scope.goToRoute(route.htmlName);
+                }
+            }
+        }
+    },
+    goToRoute: function (htmlName) {
+        (function(scope) { 
+            var url = 'views/' + htmlName,
+                xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    scope.rootElem.innerHTML = this.responseText;
+                }
+            };
+            xhttp.open('GET', url, true);
+            xhttp.send();
+        })(this);
+    }
+};
+
+
+'use strict';
+
+(function () {
+    function init() {
+        var router = new Router([
+            new Route('home', 'home.html', true),            
+            new Route('about', 'about.html')
+        ]);
+    }
+    init();
+}());
+
+
 window.onload=function(){function a(a,b){var c=/^(?:file):/,d=new XMLHttpRequest,e=0;d.onreadystatechange=function(){4==d.readyState&&(e=d.status),c.test(location.href)&&d.responseText&&(e=200),4==d.readyState&&200==e&&(a.outerHTML=d.responseText)};try{d.open("GET",b,!0),d.send()}catch(f){}}var b,c=document.getElementsByTagName("*");for(b in c)c[b].hasAttribute&&c[b].hasAttribute("include")&&a(c[b],c[b].getAttribute("include"))};
+
