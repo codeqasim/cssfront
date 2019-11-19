0f3fc7ad6d3022c619bca07e3259713dc39e1c50
Qasim Hussain <44278286+qaxim@users.noreply.github.com>
2019-11-19 09:31:02 +0500

    p

--- assets/js/app.js
+++ assets/js/app.js
@@ -100,6 +100,7 @@ Router.prototype = {
         var router = new Router([
             new Route('home', 'home.html', true),            
             new Route('about', 'about.html'),
+            new Route('form-input', 'form-input.html'),
             new Route('form-checkbox', 'form-checkbox.html'),
             new Route('layout', 'layout.html'),
             new Route('layout-grid', 'layout-grid.html')
@@ -110,4 +111,3 @@ Router.prototype = {
 
 
 window.onload=function(){function a(a,b){var c=/^(?:file):/,d=new XMLHttpRequest,e=0;d.onreadystatechange=function(){4==d.readyState&&(e=d.status),c.test(location.href)&&d.responseText&&(e=200),4==d.readyState&&200==e&&(a.outerHTML=d.responseText)};try{d.open("GET",b,!0),d.send()}catch(f){}}var b,c=document.getElementsByTagName("*");for(b in c)c[b].hasAttribute&&c[b].hasAttribute("include")&&a(c[b],c[b].getAttribute("include"))};
-
