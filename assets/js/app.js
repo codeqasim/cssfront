ee61645cd121b517f4be199bcfe4acd470178149
Qasim Hussain <compoxition@gmail.com>
2019-11-29 08:19:44 +0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,17 +2,14 @@ var app = angular.module('app', ['ngRoute','ngMeta']);
 
 app.config(['$locationProvider', '$routeProvider','ngMetaProvider', function ($locationProvider, $routeProvider,ngMetaProvider) {
 
-
-
-
 $routeProvider.
 
 when('/home', {
 templateUrl: './views/home.html',
  data: {
       meta: {
-        'title': 'Booknow - Flights - Hotels - Cars and Tours ',
-        'description': 'Book now is the best source for your online travel booking business. we offer last minute travel services such as flights hotels rooms tours and cars rental or transfer '
+        'title': 'CSSFront Inspired by Google Material ',
+        'description': 'The only frontend framework you ever need to design your beautifull web apps'
       }
     }
 }).
@@ -76,24 +73,20 @@ templateUrl: './views/headdrop.html',
 
 app.controller('Home', ['$http','$scope',function ($http,$scope) {
 
-     $http({
-     method: 'GET',
-     url: 'https://www.phptravels.net/public/expedia/api/expedia/list?appKey=phptravels',
-     data: $.param({ }),
-     headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-     }).then(
-     function(res) {
+ //     $http({
+ //     method: 'GET',
+ //     url: 'https://www.phptravels.net/public/expedia/api/expedia/list?appKey=phptravels',
+ //     data: $.param({ }),
+ //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+ //     }).then(
+ //     function(res) {
 
-     $scope.items = res.data.HotelListResponse.HotelList.HotelSummary
-     console.log('succes !',  res.data.HotelListResponse.HotelList.HotelSummary);
-     },
-     function(err) {
-     console.log('error...', err);
-     }
- );
+ //     $scope.items = res.data.HotelListResponse.HotelList.HotelSummary
+ //     console.log('succes !',  res.data.HotelListResponse.HotelList.HotelSummary);
+ //     },
+ //     function(err) {
+ //     console.log('error...', err);
+ //     }
+ // );
 
 }]);
-
-
-
-
